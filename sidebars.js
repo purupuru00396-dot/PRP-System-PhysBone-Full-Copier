1bff4e53036da3509175d9466ecde110737e71d9
PURUPURU <purupuru00396@gmail.com>
2026-01-13 07:05:30 +0900

    fix: expand サポート情報 category by default

--- sidebars.js
+++ sidebars.js
@@ -17,22 +17,17 @@ const sidebars = {
                     label: 'サポート情報',
                     collapsible: true,
                     collapsed: false,
-                    items: [
-                        'pbfc/faq',
-                        'pbfc/license',
-                        'pbfc/environment',
-                    ],
+                    items: ['pbfc/faq', 'pbfc/license', 'pbfc/environment'],
                 },
+            ],
+        }, 
 
         {
             type: 'category',
             label: 'Copy Failure Inspector Tools Manual',
             collapsible: true,
             collapsed: false,
-            items: [
-                'cfi/merits',
-                'cfi/getting-started',
-            ],
+            items: ['cfi/merits', 'cfi/getting-started'],
         },
     ],
 };
